b8a68161e1a1aa813c1f3d96a6b3c4f349ff550f
Sophie Kirschner <sophiek@pineapplemachine.com>
2023-06-03 00:40:27 +0300

    Add missing constructors to StackAllocator

--- src/scamp/alloc/stack.d
+++ src/scamp/alloc/stack.d
@@ -20,6 +20,26 @@ struct StackAllocator {
     
     @disable this(this);
     
+    this(size_t buffer_size, bool buffer_owned, void* buffer) {
+        this.buffer_size = buffer_size;
+        this.buffer = buffer;
+        this.next = this.buffer;
+        this.buffer_owned = buffer_owned;
+        assert(this.ok);
+    }
+    
+    this(size_t buffer_size) {
+        this(buffer_size, true, malloc(buffer_size));
+    }
+    
+    this(T, size_t size)(T[size] buffer) {
+        this(size * T.sizeof, false, buffer.ptr);
+    }
+    
+    ~this() {
+        this.destroy();
+    }
+    
     /// When freeing a pointer, any pointers that were allocated
     /// after the one being freed are also freed.
     void free(void[] buffer) {
